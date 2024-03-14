e65b3379a624cedd4ffc39381a087e8eb57e1744
BNDou <942490898@qq.com>
2024-03-14 21:53:25 +0800

    更新

--- SpeedServerClientUpdate/SpeedClientUpdate.py
+++ SpeedServerClientUpdate/SpeedClientUpdate.py
@@ -1,7 +1,9 @@
+# -*- coding: utf-8 -*-
 import configparser
 import os.path
 import socket
 import sys
+import time
 
 if __name__ == "__main__":
     # 初始化配置文件路径
@@ -9,24 +11,25 @@ if __name__ == "__main__":
     curpath = os.path.dirname(os.path.realpath(sys.argv[0]))
     # 配置文件路径（组合、相对路径）
     inipath = os.path.join(curpath, "DirSvrInfo1.ini")
-    print("当前地址：", inipath)
+    print("\n当前配置文件地址：", inipath)
     # 数据读取
     conf = configparser.ConfigParser()
     conf.read(inipath, encoding="utf-8")
     # domain = conf.get(conf.sections()[0], 'domain')
-    domain = '域名'
+    domain = ''  # 填写域名地址
 
-    print("1、IP开始获取")
+    print("\n1、IP开始获取")
     myIp = socket.getaddrinfo(domain, 'http')[0][4][0]
-    print("获取到IP=" + myIp)
+    print("最新IP=" + myIp)
 
-    print("2、修改IP到DirSvrInfo1.ini配置文件中")
+    print("\n2、修改IP到DirSvrInfo1.ini配置文件中")
     # 修改 ip 配置信息
     conf.set('ServerInfo1', 'IP', myIp)
     conf.set('ServerInfo1', 'Domain', domain)
 
-    print("3、保存更新DirSvrInfo1.ini配置文件")
+    print("\n3、保存更新DirSvrInfo1.ini配置文件")
     # 保存修改后的配置信息到 ini 文件
     conf.write(open(inipath, "r+", encoding="utf-8"))  # r+模式
 
-    print("更新完成，请关闭！")
+    print("\n更新完成！\n【请手动关闭,或5秒后自动关闭】")
+    time.sleep(5)
--- SpeedServerClientUpdate/SpeedServerUpdate.py
+++ SpeedServerClientUpdate/SpeedServerUpdate.py
@@ -1,48 +1,70 @@
+# -*- coding: utf-8 -*-
 import os.path
 import socket
 import sys
+import time
 
+import psutil as p
 import requests
 import yaml
 
 if __name__ == "__main__":
+    log = ""
     # 初始化配置文件路径
     # 当前文件路径
     curpath = os.path.dirname(os.path.realpath(sys.argv[0]))
     # 配置文件路径（组合、相对路径）
     inipath = os.path.join(curpath, "config.yml")
-    print("当前地址：", inipath)
+    log += f'当前配置文件地址： {inipath}\n'
     # 数据读取
+    log += "读取配置文件...\n"
     with open('config.yml', 'r', encoding='gbk') as f:
         config = yaml.safe_load(f)
     ServerIP = config['Server']['IP']
     QQServerIP = config['QQServer']['IP']
+    log += f'ServerIP={ServerIP}\nQQServerIP={QQServerIP}\n\n'
 
-    print("1、IP开始获取")
-    domain = '域名'
+    log += "1、开始获取最新IP\n"
+    domain = ''  # 填写域名地址
     myIP = socket.getaddrinfo(domain, 'http')[0][4][0]
-    print(f'获取到IP={myIP}')
+    log += f'最新IP={myIP}\n'
 
-    if any(s != myIP for s in [str(ServerIP),str(QQServerIP)]):
+    if any(s != myIP for s in [str(ServerIP), str(QQServerIP)]):
         # myIP != ServerIP and myIP != QQServerIP:
-        print("2、修改IP到config.yml配置文件中")
+        log += "2、修改IP到config.yml配置文件中\n"
         # 修改 ip 配置信息
         config['Server'].update({'IP': myIP})
         config['QQServer'].update({'IP': myIP})
 
-        print("3、保存更新config.yml配置文件")
+        log += "3、保存更新config.yml配置文件\n"
         # 保存修改后的配置信息到 yml 文件
         with open('config.yml', 'w') as f:
             yaml.safe_dump(config, f, allow_unicode=True)
 
-        print("更新完成，请关闭！")
+        # 退出服务进程
+        for proc in p.process_iter():
+            # print(“pid-%d,name:%s” % (proc.pid,proc.name()))
+            if proc.name() == "服务端.exe":
+                if os.name == 'nt':  # Windows系统
+                    cmd = f'taskkill /pid {str(proc.pid)} /f'
+                    try:
+                        log += "退出服务\n"
+                        os.system(cmd)
+                    except Exception as e:
+                        log += e
+                    break
+        # 重启服务
+        log += "启动服务\n"
+        os.system(os.path.join(curpath, "服务端.exe"))
+
+        log += "更新完成！\n"
 
         # 通知内容
         url = "https://wxpusher.zjiecode.com/api/send/message"
         headers = {"Content-Type": "application/json"}
         data = {
             "appToken": "AT_XQGRg1wEOkgJnJ99fBT8EDmJpeIy4wnE",
-            "content": "config.yml文件IP更新为【" + myIP + "】",
+            "content": f'config.yml文件IP更新为【{myIP}】\n\nlog出输：\n{log}',
             "summary": "服务端更新",
             "contentType": 1,
             "topicIds": [27124],
@@ -52,4 +74,8 @@ if __name__ == "__main__":
         # response.encoding = "utf-8"
         # print(response.json())
     else:
-        print("无需更新IP配置，请退出！")
+        log += "IP相同无需更新配置，请退出！\n"
+
+    # 5秒后自动关闭
+    print(log + "【5秒后自动关闭】")
+    time.sleep(5)
